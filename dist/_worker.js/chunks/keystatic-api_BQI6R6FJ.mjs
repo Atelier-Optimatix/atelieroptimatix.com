2757ed39f6af0b6411e74405efd1878c56e37f94
Shamil Asitha Kuruppu <shamil.asitha@gmail.com>
2026-01-17 02:47:43 +0530

    privacy policy fixes

--- dist/_worker.js/chunks/keystatic-api_BQI6R6FJ.mjs
+++
@@ -1,6 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/keystatic-api_KyjvFBv4.mjs').then(n => n.k);
-
-export { page };
